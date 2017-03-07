0a93a10391cacb7d42ab1ab640a957b73fc72a14
Jason Voigt <joyrexus@icloud.com>
2017-03-06 22:26:28 -0500

    Remove config file, use env vars

--- index.js
+++ index.js
@@ -1,68 +1,68 @@
 'use strict';
 
-const session = require('express-session');
 const express = require('express');
+const session = require('express-session');
+const Auth = require('shopify-token');
 
-const ShopifyToken = require('..');
-const config = require('./config');
+const auth = new Auth({
+    'apiKey': process.env.SHOPIFY_API_KEY,
+    'sharedSecret': process.env.SHOPIFY_SECRET,
+    'redirectUri': 'http://localhost:8080/callback',
+    'scopes':  ['read_products', 'read_orders', 'read_customers'] 
+});
 
-const shopifyToken = new ShopifyToken(config);
 const app = express();
 
 app.use(session({
-  secret: 'eo3Athuo4Ang5gai',
-  saveUninitialized: false,
-  resave: false
+    secret: 'icanhascheezburger',
+    saveUninitialized: false,
+    resave: false
 }));
 
+
 app.get('/', (req, res) => {
-  if (req.session.token) return res.send('Token ready to be used');
 
-  //
-  // Generate a random nonce.
-  //
-  const nonce = shopifyToken.generateNonce();
+    if (req.session.token) return res.send(`Token ${req.session.token}`);
 
-  //
-  // Generate the authorization URL. For the sake of simplicity the shop name
-  // is fixed here but it can, of course, be passed along with the request and
-  // be different for each request.
-  //
-  const uri = shopifyToken.generateAuthUrl(config.shop, undefined, nonce);
+    // generate a random nonce
+    const nonce = auth.generateNonce();
 
-  //
-  // Save the nonce in the session to verify it later.
-  //
-  req.session.state = nonce;
-  res.redirect(uri);
+    // generate the authorization URL
+    const uri = auth.generateAuthUrl(req.query.shop, undefined, nonce);
+
+    // save nonce in session for later verification
+    req.session.state = nonce;
+    res.redirect(uri);
 });
+
 
 app.get('/callback', (req, res) => {
-  const state = req.query.state;
 
-  if (
-      typeof state !== 'string'
-    || state !== req.session.state          // Validate the state.
-    || !shopifyToken.verifyHmac(req.query)  // Validare the hmac.
-  ) {
-    return res.status(400).send('Security checks failed');
-  }
+    const state = req.query.state;
 
-  //
-  // Exchange the authorization code for a permanent access token.
-  //
-  shopifyToken.getAccessToken(req.query.shop, req.query.code)
-    .then((token) => {
-      console.log(token);
+    if (
+        typeof state !== 'string'   ||
+        state !== req.session.state ||  // validate state
+        !auth.verifyHmac(req.query)     // validate hmac
+    ) {
+        return res.status(400).send('Security check failed');
+    }
 
-      req.session.token = token;
-      req.session.state = undefined;
-      res.redirect('/');
-    })
-    .catch((err) => {
-      console.error(err.stack);
-      res.status(500).send('Oops, something went wrong');
-    });
+    // exchange auth code for permanent access token
+    auth.getAccessToken(req.query.shop, req.query.code)
+        .then((token) => {
+
+            console.log(token);
+
+            req.session.token = token;
+            req.session.state = undefined;
+            res.redirect('/');
+        })
+        .catch((err) => {
+
+            console.error(err.stack);
+            res.status(500).send(err);
+        });
 });
 
-app.listen(8080, () => console.log('Open http://localhost:8080 in your browser'));
+app.listen(8080, () => console.log('listening at http://localhost:8080'));
